4081ce8696e1aa37f0bfc2745a70e9a36c3c5a0c
José Walisson <josewalisonpessoadelima@gmail.com>
2023-01-31 01:57:52 -0300

    Finalização do projeto

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ class Calculator {
     // Precess all calculator operations
     processOperation(operation) {
         // Check if current value is empty
-        if (this.currentOperationText.innerText === '') {
+        if (this.currentOperationText.innerText === '' && operation !== 'C') {
             // Change operation
             if (this.previousOperationText.innerText !== '') {
                 this.changeOperation(operation);
@@ -54,6 +54,18 @@ class Calculator {
                 operationValue = previous * current;
                 this.updateScreen(operationValue, operation, current, previous);
                 break;
+            case 'DEL':
+                this.processDelOperator();
+                break;
+            case 'CEL':
+                this.processClearCurrentOperation();
+                break;
+            case 'C':
+                this.processClearOperation();
+                break;
+            case '=':
+                this.processEqualOperator();
+                break;
             default:
                 return;
         }
@@ -80,6 +92,40 @@ class Calculator {
         }
     }
 
+    // Change math operation
+    changeOperation(operation) {
+        const mathOperations = ['*', '/', '+', '-'];
+        
+        if (!mathOperations.includes(operation)) {
+            return;
+        }
+
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
+    }
+
+    // Delete the last digit
+    processDelOperator() {
+        this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1);
+    }
+
+    // Clear current operation
+    processClearCurrentOperation() {
+        this.currentOperationText.innerText = '';
+    }
+    
+    // Clear all operations
+    processClearOperation() {
+        this.currentOperationText.innerText = '';
+        this.previousOperationText.innerText = '';
+    }
+
+    // Process an operation
+    processEqualOperator() {
+        const operation = previousOperationText.innerText.split(' ')[1];
+
+        this.processOperation(operation);
+    }
+
 }
 
 const calc = new Calculator(previousOperationText, currentOperationText);
@@ -94,4 +140,4 @@ buttons.forEach((btn) => {
             calc.processOperation(value);
         }
     });
-})
+});
